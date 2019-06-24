df5448c90df76fcae8761da302cbb8cae94e79c4
Crystall1time <48089698+Crystall1time@users.noreply.github.com>
2019-06-24 16:05:43 +0300

    My test task.

---
+++ Test_task/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, 6, 7, 8, 9, 10, 11 };
+            Console.WriteLine(bsearch(arr, 12, 7)); //Example
+            Console.ReadKey();
+        }
+        static int bsearch(int[] arr, int x, int n)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                if (arr[i] > x)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
